ad403521c2ebb07db3d2ba42c5b46ad441e046b5
Marc Osherson <osherson@hexcms.hexfarm.rutgers.edu>
2019-11-05 15:47:58 -0500

    typo

--- analysis/Cleanup.py
+++ analysis/Cleanup.py
@@ -4,4 +4,4 @@ import TEMPPAYLOAD
 from TEMPPAYLOAD import *
 import os
 
-os.systme("convert ../results/"+NAME+"/*.png ../results/"+NAME+"/REPORT.pdf")
+os.system("convert ../results/"+NAME+"/*.png ../results/"+NAME+"/REPORT.pdf")
